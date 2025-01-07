cb75e7abadb852a5898db3595948719b74ef8660
CantCode023 <cantcode023@gmail.com>
2025-01-08 01:39:44 +0800

    change from seperating proxy list to only one list since the seperation is handled in proxy_pool.py

--- modules/proxy/scraper/main.py
+++ modules/proxy/scraper/main.py
@@ -9,34 +9,16 @@ http_sources = config["http"]["sources"]
 socks4_sources = config["socks4"]["sources"]
 socks5_sources = config["socks5"]["sources"]
 
-def scrape_proxies(type:Literal["socks4", "socks5", "http", "all"]="all"):
-    http_proxies, socks4_proxies, socks5_proxies = [], [], []
-    for source in http_sources:
+def scrape_proxies():
+    proxies = []
+    for source in http_sources+socks4_sources+socks5_sources:
         try:
             response = requests.get(source)
-            proxies = response.text.strip().split("\n")
-            http_proxies.extend(proxies)
+            if response.status_code == 200:
+                for proxy in response.text.splitlines():
+                    proxy = proxy.strip()
+                    if proxy:
+                        proxies.append(proxy)
         except:
             continue
-    for source in socks4_sources:
-        try:
-            response = requests.get(source)
-            proxies = response.text.strip().split("\n")
-            socks4_proxies.extend(proxies)
-        except:
-            continue
-    for source in socks5_sources:
-        try:
-            response = requests.get(source)
-            proxies = response.text.strip().split("\n")
-            socks5_proxies.extend(proxies) 
-        except:
-            continue 
-    if type == "all":
-        return http_proxies+socks4_proxies+socks5_proxies
-    elif type == "http":
-        return http_proxies
-    elif type == "socks4":
-        return socks4_proxies
-    elif type == "socks5":
-        return socks5_proxies
+    return proxies
