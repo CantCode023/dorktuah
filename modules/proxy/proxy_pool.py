b9d3b85803dd0949e9bbe8d9b35bb4cc607ec39c
CantCode023 <cantcode023@gmail.com>
2025-01-08 01:40:41 +0800

    implemented scrape_proxy feature in proxy_pool.py

--- modules/proxy/proxy_pool.py
+++ modules/proxy/proxy_pool.py
@@ -1,9 +1,9 @@
 from typing import Literal
-from bs4.element import nonwhitespace_re
-import random, requests
 from pathlib import Path
+from .scraper import scrape_proxies
+import random, requests
 
-def ProxyPool(type:Literal["socks4", "socks5", "http", "all"] = "all"):
+def ProxyPool(type:Literal["socks4", "socks5", "http", "all"] = "all", use_custom:bool=True):
     def __get_proxies():
         with open(Path(__file__).parent/"proxies.txt", "r") as f:
             proxies = [line.strip() for line in f.readlines()]
@@ -21,7 +21,7 @@ def ProxyPool(type:Literal["socks4", "socks5", "http", "all"] = "all"):
             return False
         return False 
         
-    proxies = __get_proxies()
+    proxies = __get_proxies() if use_custom else scrape_proxies()
     
     if type != "all":
         proxies = list(filter(lambda x: x.startswith(type), proxies))
