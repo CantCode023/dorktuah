73b27b5a662f2a73bea6612f3269c1302380f0ee
CantCode023 <cantcode023@gmail.com>
2025-01-09 17:02:16 +0800

    added source_limit implementation

--- dorktuah/modules/proxy/proxy_pool.py
+++ dorktuah/modules/proxy/proxy_pool.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from .scraper import scrape_proxies
 import random, requests
 
-def ProxyPool(type:Literal["socks4", "socks5", "http", "all"] = "all", use_custom:bool=True):
+def ProxyPool(type:Literal["socks4", "socks5", "http", "all"] = "all", use_custom:bool=True, source_limit:int=10):
     def __get_proxies():
         with open(Path(__file__).parent/"proxies.txt", "r") as f:
             proxies = [line.strip() for line in f.readlines()]
@@ -21,7 +21,7 @@ def ProxyPool(type:Literal["socks4", "socks5", "http", "all"] = "all", use_custo
             return False
         return False 
         
-    proxies = __get_proxies() if use_custom else scrape_proxies()
+    proxies = __get_proxies() if use_custom else scrape_proxies(source_limit)
     
     if type != "all":
         proxies = list(filter(lambda x: x.startswith(type), proxies))
@@ -32,6 +32,6 @@ def ProxyPool(type:Literal["socks4", "socks5", "http", "all"] = "all", use_custo
         print("[!] Trying", proxy)
         if __check(proxy):
             print("[!] Found working!")
-            return {"http": proxy, "https": proxy}
+            return proxy
         i+=1
         print("[!] Failed!")
