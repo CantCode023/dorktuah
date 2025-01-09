ec983b68fb553c3685061bf77b781658699a19f7
CantCode023 <cantcode023@gmail.com>
2025-01-09 17:01:00 +0800

    added source_limit to proxy scraping so it doesn't take too long

--- dorktuah/modules/proxy/scraper/main.py
+++ dorktuah/modules/proxy/scraper/main.py
@@ -8,17 +8,22 @@ with open(Path(__file__).parent/"config.toml", "r") as f:
 http_sources = config["http"]["sources"]
 socks4_sources = config["socks4"]["sources"]
 socks5_sources = config["socks5"]["sources"]
+all_sources = http_sources+socks4_sources+socks5_sources
 
-def scrape_proxies():
+def scrape_proxies(source_limit:int=10):
     proxies = []
-    for source in http_sources+socks4_sources+socks5_sources:
+    print("[!] Scraping proxies.")
+    limited = all_sources if source_limit == None else all_sources[:source_limit]
+    for source in limited:
         try:
             response = requests.get(source)
             if response.status_code == 200:
+                print(f"[!] Scraped proxy from {source}")
                 for proxy in response.text.splitlines():
                     proxy = proxy.strip()
                     if proxy:
                         proxies.append(proxy)
         except:
             continue
+    print("[!] Scraping finished!")
     return proxies
