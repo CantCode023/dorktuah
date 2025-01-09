03856b04c69171946c3c2b2ee5ccaeb59c9db830
CantCode023 <cantcode023@gmail.com>
2025-01-09 17:09:30 +0800

    added source_limit and proxy_path implementation. Will change proxypool to class soon for class extending for easier argument implementation

--- dorktuah/dork.py
+++ dorktuah/dork.py
@@ -1,6 +1,14 @@
 from .modules.engine import Engine
+from typing import Literal
 
-def dork(query, use_proxy:bool=True, use_custom:bool=True):
-    engine = Engine(use_proxy=use_proxy, use_custom=use_custom)
+def dork(
+    query, 
+    proxy_type:Literal["socks4", "socks5", "http", "all"]="all", 
+    use_proxy:bool=True, 
+    use_custom:bool=True, 
+    source_limit:int=10,
+    proxy_path="C:/Users/cantc/Desktop/Coding/Python/dorktuah/dorktuah/modules/proxy/proxies.txt"
+):
+    engine = Engine(proxy_type=proxy_type, use_proxy=use_proxy, use_custom=use_custom, source_limit=source_limit, proxy_path=proxy_path)
     results = engine.search(query)
     return results
