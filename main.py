1ce6cbe95022dfda9acefd2dba3d3a98265f09ad
CantCode023 <cantcode023@gmail.com>
2025-01-09 17:55:54 +0800

    updated main.py to show all arguments

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-# Will add dorking later
 import dorktuah
 
 query = 'site:"youtube.com" "WINDOWS10XPRO"'
-results = dorktuah.dork(query, use_proxy=False, use_custom=True)
+results = dorktuah.dork(
+    query=query,
+    proxy_type="all",
+    use_proxy=True,
+    use_custom=True,
+    proxy_path="path_to_proxy",
+    source_limit=10
+)
 print(results)
